ab898c4519d5df64d9ec272ebb5d9d285d4d6a25
rhuebner76 <russell.j.huebner@outlook.com>
2020-01-23 09:10:57 -0800

    Corrected namespacing

--- src/ToDoApp.Wpf/App.xaml.cs
+++ src/ToDoApp.Wpf/App.xaml.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace SPR2020_CSC200_ToDo.Wpf
+namespace ToDoApp.Wpf
 {
     /// <summary>
     /// Interaction logic for App.xaml
