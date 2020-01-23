20cbde76167ab4ebe80863e053ef9b88c2366478
rhuebner76 <russell.j.huebner@outlook.com>
2020-01-23 12:10:20 -0800

    Added task model

---
+++ src/ToDoApp.Wpf/Models/TodoTask.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp.Wpf.Models
+{
+    class TodoTask
+    {
+        public string Description { get; set; }
+    }
+}
