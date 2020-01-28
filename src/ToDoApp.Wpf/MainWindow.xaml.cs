3f799ccbec70f96a97c03b83bc8326860a275e20
rhuebner76 <russell.j.huebner@outlook.com>
2020-01-28 10:45:21 -0800

    Added remove todo task button and implementation

--- src/ToDoApp.Wpf/MainWindow.xaml.cs
+++ src/ToDoApp.Wpf/MainWindow.xaml.cs
@@ -21,5 +21,23 @@ namespace ToDoApp.Wpf
             TodoTaskListView.Items.Add(item);
         }
 
+        private void OnRemoveTodoTaskButtonClick(object sender, RoutedEventArgs e)
+        {
+            int index = TodoTaskListView.SelectedIndex;
+            if (CanRemoveTodoTask(index))
+                TodoTaskListView.Items.RemoveAt(index);
+        }
+
+        private void OnTodoTaskListViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = TodoTaskListView.SelectedIndex;
+            RemoveTodoTaskButton.IsEnabled = CanRemoveTodoTask(index);
+        }
+
+        private bool CanRemoveTodoTask(int selectedIndex)
+        {
+            // selected index to remove cannot be less than 0 or greater than item count
+            return (selectedIndex >= 0 && selectedIndex < TodoTaskListView.Items.Count);
+        }
     }
 }
